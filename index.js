803053c87f3e40c6cf1816ad48e927967820c8f9
Cameron Banowsky <cameron@hack.cd>
2016-08-17 10:48:31 -0700

    firebase

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var __dirname
-
+v
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname +'/app/index.html'));
 });
